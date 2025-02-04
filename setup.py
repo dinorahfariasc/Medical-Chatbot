0cdf27b8e7ad4fea631efa8f4ec5713e6f42e258
Dinorah Farias <dinorahfarias@outlook.com>
2025-02-04 16:14:12 -0300

    :package: build: folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mediboti',
+    version='0.0.0',
+    author= 'Dinorah Farias',
+    packages=find_packages(),
+    description='Medical ChatBOt with Generative AI Project ',
+)
+
